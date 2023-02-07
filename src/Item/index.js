677f4692c164c5d129b7a70364b57d30b8096459
mahtab-darman <mahtabdarman14@gmail.com>
2023-02-07 18:26:27 +0330

    v-1.1

--- src/Item/index.js
+++ src/Item/index.js
@@ -2,13 +2,24 @@ import './style.css';
 
 
 const Item = ()=>{
-    const NumArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    const NumArray = [
+        'کار 1',
+        "کار 2",
+        "کار 3",
+        "خیلیییی خیلیییییی خیلییییییی خیلییی",
+        "کار 5",
+        "کار 6",
+        "کار 7",
+        "کار 8",
+        "کار 9",
+        "کار 10"
+    ];
 
     return (
     <section>
         {NumArray.map((element)=>(
             <div className='item'>
-                <input type="text" key={element} defaultValue={`کار ${element}`} />
+                <input type="text" key={element} defaultValue= {element} />
                 <i>X</i>
             </div>
         ))}
